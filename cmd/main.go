76b334f016037cf8af99e692bffe311431b8b7fe
loonoboy <tzimlov.max@yandex.ru>
2025-05-21 14:58:54 +0300

    add logger

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,28 @@ package main
 
 import (
 	"API_Service/internal/config"
-	"fmt"
+	"go.uber.org/zap"
+)
+
+const (
+	envProd = "prod"
+	envDev  = "dev"
 )
 
 func main() {
 	cfg := config.MustLoad()
-	fmt.Println(cfg)
+	log := setupLogger(cfg.Env)
+	log.Info("starting service", zap.String("config", cfg.Env))
+	log.Debug("debug message are enable")
+}
+
+func setupLogger(env string) *zap.Logger {
+	var log *zap.Logger
+	switch env {
+	case envDev:
+		log, _ = zap.NewDevelopment()
+	case envProd:
+		log, _ = zap.NewProduction()
+	}
+	return log
 }
